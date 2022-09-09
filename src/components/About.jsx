f99541bfb7aa3a3949e43ffc6360c601518bd274
Ricardo Pereira <ricardo.hpl@gmail.com>
2022-09-09 19:37:35 -0300

    Commit About Component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,8 +12,10 @@ const About = () => {
     <Flex flexDir={'column'} w='95%'>
       <TitleSections>Ricardo Pereira</TitleSections>
       <Box paddingX={['25px', '25px', '45px']} w={['100%']}>
-        <Text marginY={4}>Formado na Fatec-SP, tenho mais de 12 anos de experiência na area de TI trabalhando com diferentes papéis. Atualmente estou trabalhando como
-          Desenvolvedor Web mas já atuei como Administrador de Dados, Líder de Equipe, Product Owner e Analista de Negócios\Requisitos.
+        <Text marginY={4}>Formado na Fatec-SP, tenho mais de 12 anos de experiência na area de TI trabalhando com diferentes
+          papéis. Atualmente estou trabalhando como
+          Desenvolvedor Web mas já atuei como Administrador de Dados, Líder de Equipe,
+          Product Owner e Analista de Negócios\Requisitos.
         </Text >
         <Text marginY={4}>Decidi focar como desenvolvedor pois adoro resolver problemas e codificar a solução, apesar de gostar também de realizar o levantamento de
           requisitos e definições das regras de negócio com clientes.
